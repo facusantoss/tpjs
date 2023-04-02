28eb516190e8b06150557fb6e1be095f28b54476
Facundo Santos <facusantos93@gmail.com>
2023-04-02 19:43:15 -0300

    modificaciones necesarias para el segundo tp

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-
-let dniUsuario= "37863939";
+let dniUsuario = "37863939";
 let ingreso= false;
 
 alert ("Bienvenido/a al cotizador");
@@ -14,39 +13,75 @@ for (let i = 2; i >= 0; i--) {
     } 
 }
 
+
 if(ingreso){
-function Auto(marca, modelo, anio){
-    this.marca=marca;
-    this.modelo=modelo;
-    this.anio=anio;
-} 
+const vehiculos =[
+    {id: 1, marca: "toyota", modelo: "etios", anio: 2018},
+    {id: 2, marca: "peugeot", modelo: "208", anio: 2017},
+    {id: 3, marca: "fiat", modelo: "punto", anio: 2013},
+    {id: 4, marca: "renault", modelo: "kwid", anio: 2019},
+    {id: 5, marca: "toyota", modelo: "yaris", anio: 2020},
+];
+
+vehiculos.forEach(function(vehiculo, indice) {
+  console.log("Vehículo " + (indice+1) + ": Marca: " + vehiculo.marca + ", Modelo: " + vehiculo.modelo);
+});
+
+const toyotas = vehiculos.filter(vehiculo => vehiculo.marca === "toyota");
+console.log(toyotas);
+
+function nuevoAuto(marca, modelo, anio){
+    this.id=vehiculos.length + 1;
+    this.marca= marca;
+    this.modelo= modelo;
+    this.anio= anio;
+}
+
+const validarAnio = (anio) => /^\d{4}$/.test(anio);
 
 let marca= prompt("Ingresa la marca");
 let modelo= prompt("ingresá el modelo");
 let anio= prompt("ingresá el año del vehículo");
 
-const autoCliente= new Auto(marca, modelo, anio);
+
+while (!validarAnio(anio)) {
+  alert("El año ingresado es inválido. Por favor, ingresá un año válido.");
+  anio = prompt("ingresá el año del vehículo");
+}
+
+const autoCliente= new nuevoAuto(marca, modelo, anio);
+vehiculos.push(autoCliente);
 console.log(autoCliente);
 
 
-let costoRC= 3000;
-let costoTC= 8000;
-let costoTR= 14000;
+function calcularCostoRC() {
+    const costo = 3000;
+    alert("Seleccionaste Responsabilidad Civil");
+    alert("Tu costo es de $ " + costo);
+  }
+  
+  function calcularCostoTC() {
+    const costo = 8000;
+    alert("Seleccionaste Terceros Completo");
+    alert("Tu costo es de $ " + costo);
+  }
+  
+  function calcularCostoTR() {
+    const costo = 14000;
+    alert("Seleccionaste Todo Riesgo");
+    alert("Tu costo es de $ " + costo);
+  }
 
 let opcion= prompt("Seleccioná una de las coberturas \n1-Responsabilidad Civil \n2-Terceros Completo \n3-Todo Riesgo")
 switch (opcion) {
     case "1":
-        alert("Seleccionaste Responsabilidad Civil");
-        alert("Tu costo es de $ " + costoRC);
-
+        calcularCostoRC();
     break;
     case "2":
-        alert("Seleccionaste Terceros Completo"); 
-        alert("Tu costo es de $ " + costoTC);   
+       calcularCostoTC();   
     break;
     case "3":
-        alert("Seleccionaste Todo Riesgo");
-        alert("Tu costo es de $ " + costoTR);
+        calcularCostoTR();
     break;
 
     default:
@@ -57,3 +92,5 @@ switch (opcion) {
 } else {
     alert(" no podes ingresar al cotizador");
 }
+
+alert("Gracias por utilizar nuestro cotizador de seguros. ¡Que tengas un buen día!");
